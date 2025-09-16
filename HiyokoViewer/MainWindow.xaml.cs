2a1058315158738a296f66914b9a921bff1721be
MasakiTakano <lambert_phong@yahoo.co.jp>
2025-09-17 04:07:51 +0900

    ダークテーマ

--- HiyokoViewer/MainWindow.xaml.cs
+++ HiyokoViewer/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media.Imaging;
+using System.Windows.Interop;
+using System.Windows.Forms;
+using MaterialDesignThemes.Wpf.AddOns.Utils.Screen; // 追加
 
 namespace HiyokoViewer
 {
@@ -14,18 +18,20 @@ namespace HiyokoViewer
 
         private void OpenFileButton_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            // 対応するファイル形式のフィルタを設定
-            openFileDialog.Filter = "画像ファイル (*.png;*.jpg;*.jpeg;*.bmp;*.gif)|*.png;*.jpg;*.jpeg;*.bmp;*.gif|全てのファイル (*.*)|*.*";
+			var openFileDialog = new OpenFileDialog
+			{
+				// 対応するファイル形式のフィルタを設定
+				Filter = "画像ファイル (*.png;*.jpg;*.jpeg;*.bmp;*.gif)|*.png;*.jpg;*.jpeg;*.bmp;*.gif|全てのファイル (*.*)|*.*"
+			};
 
-            if (openFileDialog.ShowDialog() == true)
+			if (openFileDialog.ShowDialog() == true)
             {
                 // 選択されたファイルのパスから画像を読み込む
                 string filePath = openFileDialog.FileName;
                 try
                 {
                     // BitmapImageを使って画像を読み込み、Imageコントロールに表示
-                    BitmapImage bitmap = new BitmapImage();
+                    var bitmap = new BitmapImage();
                     bitmap.BeginInit();
                     bitmap.UriSource = new Uri(filePath);
                     bitmap.CacheOption = BitmapCacheOption.OnLoad; // ファイルをロックしないようにすぐに読み込む
@@ -38,6 +44,12 @@ namespace HiyokoViewer
                     MessageBox.Show("画像の読み込みに失敗しました: " + ex.Message);
                 }
             }
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
         }
     }
 }
